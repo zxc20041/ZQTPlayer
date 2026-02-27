9043094a382b3bf6c70f14a10f48130579e5f42a
xc z <13521366337@163.com>
2026-02-27 13:58:42 +0800

    添加英文和中文语言文件，更新.gitignore以包含i18n目录

---
+++ i18n/app_en_US.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Main</name>
+        <message>
+            <source>Hello World</source>
+            <translation>Hello World</translation>
+        </message>
+        <message>
+            <source>Button</source>
+            <translation>Button</translation>
+        </message>
+        <message>
+            <source>Hello!</source>
+            <translation>Hello!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Home</name>
+        <message>
+            <source>ZQTPlayer</source>
+            <translation>ZQTPlayer</translation>
+        </message>
+        <message>
+            <source>Settings</source>
+            <translation>Settings</translation>
+        </message>
+    </context>
+    <context>
+        <name>Settings</name>
+        <message>
+            <source>General</source>
+            <translation>General</translation>
+        </message>
+        <message>
+            <source>Theme</source>
+            <translation>Theme</translation>
+        </message>
+        <message>
+            <source>Color mode</source>
+            <translation>Color mode</translation>
+        </message>
+        <message>
+            <source>Light</source>
+            <translation>Light</translation>
+        </message>
+        <message>
+            <source>Dark</source>
+            <translation>Dark</translation>
+        </message>
+        <message>
+            <source>Follow System</source>
+            <translation>Follow System</translation>
+        </message>
+        <message>
+            <source>Style</source>
+            <translation>Style</translation>
+        </message>
+        <message>
+            <source>Application Style</source>
+            <translation>Application Style</translation>
+        </message>
+        <message>
+            <source>Restart to apply</source>
+            <translation>Restart to apply</translation>
+        </message>
+        <message>
+            <source>Language</source>
+            <translation>Language</translation>
+        </message>
+        <message>
+            <source>Display Language</source>
+            <translation>Display Language</translation>
+        </message>
+    </context>
+</TS>
---
+++ i18n/app_zh_CN.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>Main</name>
+        <message>
+            <source>Hello World</source>
+            <translation>你好，世界</translation>
+        </message>
+        <message>
+            <source>Button</source>
+            <translation>按钮</translation>
+        </message>
+        <message>
+            <source>Hello!</source>
+            <translation>你好！</translation>
+        </message>
+    </context>
+    <context>
+        <name>Home</name>
+        <message>
+            <source>ZQTPlayer</source>
+            <translation>ZQTPlayer</translation>
+        </message>
+        <message>
+            <source>Settings</source>
+            <translation>设置</translation>
+        </message>
+    </context>
+    <context>
+        <name>Settings</name>
+        <message>
+            <source>General</source>
+            <translation>常规</translation>
+        </message>
+        <message>
+            <source>Theme</source>
+            <translation>主题</translation>
+        </message>
+        <message>
+            <source>Color mode</source>
+            <translation>颜色模式</translation>
+        </message>
+        <message>
+            <source>Light</source>
+            <translation>浅色</translation>
+        </message>
+        <message>
+            <source>Dark</source>
+            <translation>深色</translation>
+        </message>
+        <message>
+            <source>Follow System</source>
+            <translation>跟随系统</translation>
+        </message>
+        <message>
+            <source>Style</source>
+            <translation>样式</translation>
+        </message>
+        <message>
+            <source>Application Style</source>
+            <translation>应用样式</translation>
+        </message>
+        <message>
+            <source>Restart to apply</source>
+            <translation>重启生效</translation>
+        </message>
+        <message>
+            <source>Language</source>
+            <translation>语言</translation>
+        </message>
+        <message>
+            <source>Display Language</source>
+            <translation>显示语言</translation>
+        </message>
+    </context>
+</TS>
